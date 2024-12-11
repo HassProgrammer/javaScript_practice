52545d213ad82695b67df5ffa918e910dadbfa7a
HassProgrammer <md.ahshanulalam@gmail.com>
2024-12-11 12:20:16 +0600

    Learn about String

---
+++ 08-String/01-String.js
@@ -0,0 +1,29 @@
+const name = "Ahshanul Alam Khan"
+const repoCount = 50
+// console. log (name + repoCount + " Value");
+console.log (`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('Ahshanul Alam Khanhan')
+// console.log (gameName [01) ;
+// console.log (gameName._proto_;
+// console.log (gameName.length);
+// console.log (gameName.toUpperCase());
+console.log (gameName.charAt (2));
+console.log (gameName.indexOf('A'));
+
+const newString = gameName.substring (0, 4)
+console.log (newString) ;
+const anotherString = gameName.slice (-8, 4)
+console.log (anotherString) ;
+
+
+const newStringOne = "   Khan    "
+console.log (newStringOne);
+console.log (newStringOne.trim());
+
+const url = "https://ahshanul.com/ahshanul%20alam"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('Khan'));
+console.log(gameName.split('-'));
+
