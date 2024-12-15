62c827c1d2b02b62178d7735b553209ed482fd67
HassProgrammer <md.ahshanulalam@gmail.com>
2024-12-15 14:07:38 +0600

    First of num conv

---
+++ 09-Number-and-maths/01-Number_and_maths.js
@@ -0,0 +1,29 @@
+let score  = "33"
+console.log(typeof(score));
+
+letValueInNumber = Number(score)
+console.log(typeof(letValueInNumber));
+
+let score2 = "123abc"
+let v_in_num = Number(score2)
+console.log(typeof(score2));
+
+console.log(typeof(v_in_num));
+
+console.log(v_in_num);
+
+// "33" => 33
+//"33abc" => NaN
+//true = 1; false => 0
+
+let isLoggedIn = true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "Khan" => true
+
+
+
